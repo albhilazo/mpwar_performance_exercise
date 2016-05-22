fd58430020a310e6bacaaa90046449cb3df05069
Pablo Puga <pablo.puga.git@gmail.com>
2016-05-22 10:51:44 +0200

    Added Redis memory limitation to the storage machine provision

--- resources/storage/settings.sh
+++ resources/storage/settings.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
+sudo sed -i 's/# maxmemory <bytes>/maxmemory 268435456/' /etc/redis/redis.conf
+sudo sed -i 's/# maxmemory-policy volatile-lru/maxmemory-policy volatile-lru/' /etc/redis/redis.conf
 sudo sed -i "s/$(grep bind-address /etc/mysql/my.cnf)/bind-address = 0.0.0.0/g" /etc/mysql/my.cnf
 
 sudo mysql -u root -e "CREATE USER IF NOT EXISTS 'mpwar'@'%' IDENTIFIED BY 'performance_pass'"
