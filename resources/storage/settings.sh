0a776993da31e36a0c2c97c3cd54be4aa69b66e6
Pablo Puga <pablo.puga.git@gmail.com>
2016-05-22 10:25:45 +0200

    Added the creation of the database users to the provision of the storage machine

--- resources/storage/settings.sh
+++ resources/storage/settings.sh
@@ -3,5 +3,8 @@
 sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
 sudo sed -i "s/$(grep bind-address /etc/mysql/my.cnf)/bind-address = 0.0.0.0/g" /etc/mysql/my.cnf
 
+sudo mysql -u root -e "CREATE USER IF NOT EXISTS 'mpwar'@'%' IDENTIFIED BY 'performance_pass'"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON mpwar_performance_blog.* TO 'mpwar'@'%'"
+
 sudo service mysql restart
 sudo service redis-server restart
