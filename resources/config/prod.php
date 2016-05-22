1c4d6e7ddf1de88099601585c7a7be2ebec8fe5c
Pablo Puga <pablo.puga.git@gmail.com>
2016-05-22 10:26:10 +0200

    Added the credentials to use the remote storage server

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -7,9 +7,9 @@ $app['twig.options'] = [];
 
 $app['db.options'] = [
     "driver"    => "pdo_mysql",
-    "host"      => '127.0.0.1',
-    "user"      => 'root',
-    "password"  => 'vagrantpass',
+    "host"      => '52.30.109.150',
+    "user"      => 'mpwar',
+    "password"  => 'performance_pass',
     "dbname"    => 'mpwar_performance_blog',
     "charset"   => "utf8"
 ];
@@ -26,7 +26,7 @@ $app['orm.em.options'] = [
 ];
 $app['redis.options'] = [
     "scheme" => "tcp",
-    "host"   => "127.0.0.1",
+    "host"   => "52.30.109.150",
     "port"   => 6379
 ];
 $app['assets_path'] = 'http://performance.dev/mpwar_performance_exercise/web/assets';
